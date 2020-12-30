3d65fd035d6cd408ba87926c76fbbc118c11d81a
clry2 <chris@yiu.co.uk>
2020-12-30 10:08:04 +0000

    tidy paths

--- js/typeahead.deep.js
+++ js/typeahead.deep.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     datumTokenizer: Bloodhound.tokenizers.obj.whitespace('title'),
     queryTokenizer: Bloodhound.tokenizers.whitespace,
     prefetch: {
-      url: 'scripts/deep.json',
+      url: '/js/links.json',
       ttl: 1
     }
   });
@@ -26,7 +26,7 @@ $(document).ready(function(){
         '</div>'
       ].join('\n'),
       suggestion: function(data) {
-          return '<div class="c-list"><p class="mb-0"><a href="#'+ data.cat_ref +'" class="badge c-bg-'+ data.cat_bg +' text-monospace text-uppercase">' + data.cat + '</a> <a href="' + data.url + '">' + data.title + '</a></p></div>';
+          return '<div class="c-list"><p class="mb-0"><a href="#'+ data.cat_ref.split(" ")[0] +'" class="badge c-bg-'+ data.cat_bg +' text-monospace text-uppercase">' + data.cat + '</a> <a href="' + data.url + '">' + data.title + '</a></p></div>';
       }
     }
   });  
